50b1a5edc8ca9b527209853d6157d7075d433518
1noseA <1noseA2L@gmail.com>
2022-01-23 12:02:18 +0900

    first commit

---
+++ src/main/java/com/regex/RegexQuizApplication.java
@@ -0,0 +1,13 @@
+package com.regex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegexQuizApplication.class, args);
+	}
+
+}
