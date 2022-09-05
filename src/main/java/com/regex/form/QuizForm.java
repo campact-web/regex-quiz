e31d31d95f12004a9100cd6602dbcd85c59141b1
makiharu <kitakazetoshirokuma@gmail.com>
2022-09-05 23:59:27 +0900

    コメント追記

--- src/main/java/com/regex/form/QuizForm.java
+++ src/main/java/com/regex/form/QuizForm.java
@@ -7,21 +7,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @Data
+/* クイズ登録画面の入力フォーム*/
 public class QuizForm {
 	private int id;
 	private String quiz;
 	private String answer1;
 	private String answer2;
 	private String answer3;
-	
-	/* 登録者*/
-	private String registed_by;
-	/* 作成日*/
-	private String created_at;
-	/* 更新者*/
-	private String updated_by;
-	/* 更新日*/
-	private String updated_at;
-	
-	private String is_deleted;
 }
