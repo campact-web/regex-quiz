cfc2ee4cc743edba6942045b7d77ee2203fc9b61
makiharu <kitakazetoshirokuma@gmail.com>
2022-11-27 18:12:18 +0900

    csvファイルからのクイズ一括登録機能

--- src/main/java/com/regex/dto/QuizDto.java
+++ src/main/java/com/regex/dto/QuizDto.java
@@ -1,15 +1,13 @@
-package com.regex.entity;
+package com.regex.dto;
 
-import lombok.Data;
-
-@Data
-public class Quiz {
+import lombok.Builder;
+@Builder
+public class QuizDto {
     private int id;
     private String quiz;
     private String answer1;
     private String answer2;
     private String answer3;
-//    public List<Choice> choices;
     /* 登録者*/
     private String registed_by;
     /* 作成日*/
@@ -18,6 +16,6 @@ public class Quiz {
     private String updated_by;
     /* 更新日*/
     private String updated_at;
-
+    /* 削除フラグ*/
     private String is_deleted;
 }
